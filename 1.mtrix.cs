2c634156a6a830646f3e74eabf848c762a5f995b
ddamian97 <34185736+ddamian97@users.noreply.github.com>
2019-05-19 23:19:37 +0200

    Add files via upload

---
+++ 1.mtrix.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Mtrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] m1 = new int[,]{{ 0, 1, 2, 3, 4 },
+                          { 1, 1, 2, 3, 4},
+                          { 2, 2, 2, 3, 4},
+                          { 3, 3, 3, 3, 4},
+                          { 4, 4, 4, 4, 4},
+                           };
+
+
+
+
+            int[,] m2;
+ 
+
+            Program p1 = new Program();
+            m2 = p1.MatrixOut(m1);
+
+
+
+       
+
+            Console.WriteLine(m2[0, 0]);
+            Console.WriteLine(m2[0, 1]);
+            Console.WriteLine(m2[1, 0]);
+            Console.WriteLine(m2[1, 1]);
+
+          
+
+
+        }
+
+        public int [,] MatrixOut(int [,] m1)
+        {
+            int g1 = 0; //  tworze zmienne, do ktorych bede sumowal poszczegolne elementy tablicy
+            int y1 = 0;
+            int b1 = 0;
+            int o1 = 0;
+            int[,] mOut = new int [2,2];
+            for (int i = 0; i < m1.GetLength(0); i++)   //  tworze dwie petle przechodzace po kazdym elemencie tablicy
+            {
+
+                for (int j = 0; j < m1.GetLength(1); j++)
+                {
+                    if (i < j)
+                    {
+                        if ((i + j) < m1.GetLength(1)-1)    // sprawdzam warunki (nigdy nie sumuje przekatnych)
+                        {
+                            //Console.WriteLine("green");
+                            //Console.WriteLine(i + " "+j);
+
+
+                            g1 += m1[i,j];
+                        }
+                        if ((i + j) > m1.GetLength(1)-1)
+                        {
+                            //Console.WriteLine("blue");
+                            //Console.WriteLine(i + " "+j);
+                            b1 += m1[i, j];
+
+                        }
+                    }
+                    if (i > j)
+                    {
+                        if ((i + j) < m1.GetLength(1)-1)
+                        {
+                            //Console.WriteLine("yellow");
+                            //Console.WriteLine(i + " "+j);
+                            y1 += m1[i, j];
+                        }
+                        if ((i + j) > m1.GetLength(1)-1)
+                        {
+                            //Console.WriteLine("orange");
+                            //Console.WriteLine(i + " "+j);
+                            o1 += m1[i, j];
+                        }
+                    }
+
+                }
+            }
+
+
+
+            mOut[0, 0] = g1;    //  do nowej macierzy wpisuje moje sumy
+            mOut[0, 1] = b1;
+            mOut[1, 0] = o1;
+            mOut[1, 1] = y1;
+
+            return mOut;
+        }
+    }
+}
---
+++ 2.Reverse.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(CharInversion("Agilero"));    //  nie jestem pewny, czy dobrze zrozumialem polecenie, poniewaz przyklad   
+                                                        //  byl rozwiazany inaczej
+
+
+
+    }
+
+    static String CharInversion(String sIn)
+    {
+        string sOut = "";
+        string tmp2 = "";
+        string tmp3 = "";
+        for(int i=sIn.Length-1; i>=0; i--)  //  petla przechodze od ostatniego chara do pierwszego i dodaje do nowego stringa
+        {
+            sOut += sIn[i];
+        }
+
+       for(int i=0; i<sOut.Length; i++) //  petla zamieniajaca wszystkie litery na male
+        {
+            char ch1 = sOut[i];
+
+            if(ch1>='A' && ch1 <= 'Z')  // sprawdzam czy znak jest mala litera
+            {
+                tmp3 += "" + (char)(sOut[i] + 32);
+            }
+            else
+            {
+                tmp3 += sOut[i];
+            }
+        }
+
+
+
+        char tmp1 = (char)tmp3[0];  //  pierwszy znak przypisuje do nowego chara
+        if(tmp1>='a' && tmp1<= 'z') //  sprawdzam po tablicy ASCII czy pierwszy znak jest mala litera
+        {
+            tmp2 += "" + (char)(tmp1 - 32);
+        }
+        else
+        {
+            tmp2 += "" + tmp1;
+        }
+        String sOut1 = "";
+        for(int i=1; i<sOut.Length; i++)    // do nowej zmiennej przypisuje moj odwrocony String bez pierwszej litery
+        {
+            sOut1 += tmp3[i];
+        }
+        tmp2 += sOut1;  // do mojej duzej pierwszej litery dopisuje Stringa bez pierwszego znaku
+        return tmp2;
+    }
+
+
+}
